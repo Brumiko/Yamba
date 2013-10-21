718116eaa637fc10a70ac247af3b165c32caceca
slavek <slaven.brumec@gmail.com>
2013-10-22 01:35:16 +0200

    Lint ispravci.

--- Yamba/build/source/buildConfig/debug/hr/koris/yamba/BuildConfig.java
+++ Yamba/build/source/buildConfig/debug/hr/koris/yamba/BuildConfig.java
@@ -2,6 +2,6 @@
 package hr.koris.yamba;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
 
 }
